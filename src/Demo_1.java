16438f0a8cf752d77984040abbd1d70cf35db10c
ADMIN <ADMIN@192.168.1.73>
2024-01-10 11:48:13 +0530

    4th commit

--- src/Demo_1.java
+++ src/Demo_1.java
@@ -6,7 +6,7 @@ public class Demo_1 {
 
 		System.out.println("text 1");
 		System.out.println("text 2");
-		System.out.println("text 3");
+		System.out.println("Testing");
 		System.out.println("text 485");
 		System.out.println("Job");
 		System.out.println("text 4");
